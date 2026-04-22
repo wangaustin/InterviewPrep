bfa16ce6dbe3b9c4c69bf769cbd7dd95879d54ac
wangaustin <austin.wang220@gmail.com>
2026-04-21 20:48:38 -0500

    DotNetCSharp - Deal with requests on non-existent resources

--- DotNetCSharp/GameStore/GameStore.Api/Program.cs
+++ DotNetCSharp/GameStore/GameStore.Api/Program.cs
@@ -17,7 +17,12 @@ List<GameDto> games =
 app.MapGet("/games", () => games);
 
 // GET /games/1
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+app.MapGet("/games/{id}", (int id) =>
+{
+    var game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
    .WithName(GetGameEndpointName);
 
 // POST /games
@@ -40,6 +45,12 @@ app.MapPost("/games", (CreateGameDto newGame) =>
 app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound(); // alternatively, you can create the resource if not found
+    }
+
     games[index] = new GameDto
     (
         id,
